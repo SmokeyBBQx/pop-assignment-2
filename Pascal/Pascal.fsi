a876e41e28f5c3a5cbcc292544b98102ffb4aa20
SmokeyBBQx <47431313+SmokeyBBQx@users.noreply.github.com>
2025-10-23 22:50:00 +0200

    pascal update

--- Pascal/Pascal.fsi
+++ Pascal/Pascal.fsi
@@ -1,17 +1,21 @@
 module Pascal
 
 // Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k.
-// Will cause out of bounds error if n or k are not natural numbers.
+// Will cause out of bounds error if n or k are not natural numbers. Use the optional method to safely call the function.
+// May cause stack overflow error when k > 0 and k != n when n is an extreme number.
 val pascal : int * int -> int
 
 // Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k.
-// Will cause out of bounds error if n or k are not natural numbers.
+// Will cause out of bounds error if n or k are not natural numbers. Use the optional method to safely call the function.
+// May cause stack overflow error when k > 0 and k != n when n is an extreme number.
 val pascalNoRec : int * int -> int
 
 // Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k.
 // Will return none if n or k are not natural numbers.
+// May cause stack overflow error when k > 0 and k != n when n is an extreme number.
 val pascalOptional : int * int -> option<int>
 
 // Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k.
 // Will return none if n or k are not natural numbers.
+// May cause stack overflow error when k > 0 and k != n when n is an extreme number.
 val pascalNoRecOptional : int * int -> option<int>
--- Pascal/PascalTest.fsx
+++ Pascal/PascalTest.fsx
@@ -2,6 +2,7 @@
 
 open Pascal
 
+// Examples
 printfn "%A" (pascal (4, 2))
 printfn "%A" (pascalNoRec (10, 5))
 
@@ -9,3 +10,102 @@ printfn "%A" (pascalOptional (-4, -2))
 printfn "%A" (pascalOptional (4, 2))
 printfn "%A" (pascalNoRecOptional (0, 2))
 printfn "%A" (pascalNoRecOptional (1, 1))
+
+// Tests
+let hasPropertyOne (nk: int * int) : bool =
+    let n = fst nk
+    let k = snd nk
+    match pascalOptional nk with
+    | None -> false
+    | Some result ->
+        if k = 0 || k = n then
+            result = 1
+        else
+            true
+
+let hasPropertyOneNoRec (nk: int * int) : bool =
+    let n = fst nk
+    let k = snd nk
+    match pascalNoRecOptional nk with
+    | None -> false
+    | Some result ->
+        if k = 0 || k = n then
+            result = 1
+        else
+            true
+
+let hasPropertyTwo (nk: int * int) : bool =
+    let n = fst nk
+    let k = snd nk
+    match pascalOptional nk with
+    | None -> false
+    | Some result ->
+        if k > 0 && k < n then
+            match pascalOptional (n-1, k-1), pascalOptional (n-1, k) with
+            | Some left, Some right -> result = left + right
+            | _ -> false
+        else
+            true
+
+
+let hasPropertyTwoNoRec (nk: int * int) : bool =
+    let n = fst nk
+    let k = snd nk
+    match pascalNoRecOptional nk with
+    | None -> false
+    | Some result ->
+        if k > 0 && k < n then
+            match pascalNoRecOptional (n-1, k-1), pascalNoRecOptional (n-1, k) with
+            | Some left, Some right -> result = left + right
+            | _ -> false
+        else
+            true
+
+let partition1Cases = [
+    0, 0
+    1, 0
+    5, 0
+    System.Int32.MaxValue, 0
+]
+
+let partition2Cases = [
+    0, 0
+    1, 1
+    System.Int32.MaxValue, System.Int32.MaxValue
+]
+
+let partition3Cases = [
+    2, 1
+    4, 2
+  //  System.Int32.MaxValue, 1 // Stack Overflow
+ //   System.Int32.MaxValue, 4 // Stack Overflow
+]
+
+let test testProperty testCases =
+    let testResults = List.map testProperty testCases
+    for i in testResults do
+        printfn "%b" i
+
+printfn " "
+
+printfn "Test Results"
+
+printfn " "
+
+printfn "RECURSIVE PASCEL"
+printfn "Partition 1: k = 0"
+test hasPropertyOne partition1Cases
+printfn "Partition 2: k = n"
+test hasPropertyOne partition2Cases
+printfn "Partition 3: 0 < k < n"
+test hasPropertyTwo partition3Cases
+
+printfn " "
+
+printfn "NON-RECURSIVE PASCEL"
+printfn "Partition 1: k = 0"
+test  hasPropertyOneNoRec partition1Cases
+printfn "Partition 2: k = n"
+test  hasPropertyOneNoRec partition2Cases
+printfn "Partition 3: 0 < k < n"
+test hasPropertyTwoNoRec partition3Cases
